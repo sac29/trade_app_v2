dfb7ff9da9b021ae09b77a26e413c374e3c5559e
Shivam <sc6500@gmail.com>
2018-06-28 10:40:11 +0530

    login complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'trades/index'
+  get 'login/logout'
+  post 'login/verify'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'login#index'
 end
